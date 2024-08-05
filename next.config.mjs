7289d4f409975c04bb5e52f59c412591729df426
DvirHayat <dvir.chayat@gmail.com>
2024-08-05 14:16:59 +0300

    added CORS issue to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+async rewrites(){
+    return [{
+        source:'/api/:path*',
+        destination:'https://web-course-backend-seven.vercel.app/api/:path*'
+    },
+];
+},
+
+};
 
 export default nextConfig;
