ae33ce4d88b032db5a29cba5618081700b680a97
DvirHayat <dvir.chayat@gmail.com>
2024-08-09 22:03:09 +0300

    Register OPTIONS CORS

--- pages/api/register.js
+++ pages/api/register.js
@@ -7,6 +7,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
 
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   // Ensure database connection
   await connectToDatabase();
 
