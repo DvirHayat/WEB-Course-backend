8431a658c0808c386402962ab110fa379a2ece3c
DvirHayat <dvir.chayat@gmail.com>
2024-08-26 20:07:24 +0300

    Admin get all users bugFix

--- pages/api/adminUtil.js
+++ pages/api/adminUtil.js
@@ -76,10 +76,10 @@ async function handleDeleteRequest(req, res) {
 async function handleGetRequest(req, res) {
   try {
     // Fetch all users from the database
-    const users = await getAllUsers();
+    const connections = await getAllUsers();
     
     // Send the users data as a JSON response
-    res.status(200).json(users);
+    res.status(200).json({connections});
   } catch (error) {
     console.error('Error fetching users:', error);
     res.status(500).json({ message: 'Failed to fetch users', error });
